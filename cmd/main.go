db16eb9b0ccc7bef90e4df6b358f0756a1231f8d
Henrique Chaves <canaldomonza@gmail.com>
2023-02-12 18:35:41 -0300

    feat: modify function main and running server api

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,58 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"strconv"
-	"strings"
+	"encoding/json"
+	"log"
+	"net/http"
+	"path"
+
+	"github.com/RickChaves29/url_shortener/internal/data"
+	"github.com/RickChaves29/url_shortener/internal/domain"
+	_ "github.com/lib/pq"
 )
 
-func main() {
-	fmt.Printf("genarateCode: %v\n", genarateCode(3))
-	fmt.Printf("genarateCode: %v\n", genarateCode(6))
-	fmt.Printf("genarateCode: %v\n", genarateCode(9))
+type UrlInput struct {
+	OriginUrl string `json:"originUrl"`
 }
 
-func genarateCode(len int) string {
-	var array = make([]string, len)
+type UrlOutput struct {
+	HashUrl string `json:"hashUrl"`
+}
 
-	for i := range array {
-		n := strconv.Itoa(rand.Intn(9))
-		array[i] = n
+func main() {
+	conn, err := data.ConnectionDB()
+	if err != nil {
+		log.Fatalf("database: %v", err.Error())
 	}
+	repository := data.NewRepository(conn)
+	usecase := domain.NewUsecase(repository)
+	http.HandleFunc("/api/code/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		if r.Method == "GET" {
 
-	return strings.Join(array, "")
+			hashUrl := path.Base(r.URL.Path)
+			println("path: ", hashUrl)
+			result, err := usecase.GetOriginUrlFromRedirect(hashUrl)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				log.Fatalf(err.Error())
+			}
+			http.Redirect(w, r, result, http.StatusSeeOther)
+		}
+		if r.Method == "POST" {
+			var urlInput *UrlInput
+
+			json.NewDecoder(r.Body).Decode(&urlInput)
+			hashUrl, err := usecase.CreateNewUrl(urlInput.OriginUrl)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				log.Fatal(err.Error())
+			}
+			urlOutput := UrlOutput{HashUrl: hashUrl}
+			w.WriteHeader(http.StatusCreated)
+			json.NewEncoder(w).Encode(&urlOutput)
+		}
+	})
+	println("Server runnig on http://localhost:3030/api/code")
+	http.ListenAndServe(":3030", nil)
 }
