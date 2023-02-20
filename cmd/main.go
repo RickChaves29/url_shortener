8e556151eb1a4c7d6774a46f9ef2fa21d6d406f9
Henrique Chaves <canaldomonza@gmail.com>
2023-02-20 16:23:41 -0300

    refactor: replacing all fmt to usa log add and resolve problem with fatal error

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"path"
@@ -28,7 +27,7 @@ type Response struct {
 func main() {
 	conn, err := data.ConnectionDB()
 	if err != nil {
-		log.Fatalf("database: %v", err.Error())
+		log.Printf("ERROR DATABASE: %v\n", err.Error())
 	}
 	repository := data.NewRepository(conn)
 	usecase := domain.NewUsecase(repository)
@@ -40,31 +39,33 @@ func main() {
 			result, err := usecase.GetOriginUrlFromRedirect(hashUrl)
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
-				log.Fatalf(err.Error())
+				log.Printf("ERROR: %v\n", err.Error())
 			}
 			http.Redirect(w, r, result, http.StatusSeeOther)
+			log.Println("GET/: redirect to original URL")
 		}
 		if r.Method == "POST" {
 			var urlInput *UrlInput
 			json.NewDecoder(r.Body).Decode(&urlInput)
 			result, err := regexp.MatchString(`(?:https?:\/\/)`, urlInput.OriginUrl)
 			if err != nil {
-				fmt.Printf("regex error: %v", err.Error())
+				log.Printf("ERROR REGEX: %v", err.Error())
 			}
 			if !result {
-				fmt.Printf("warn: url format is incorrect")
 				w.WriteHeader(http.StatusBadRequest)
 				message := Response{Message: "url format is incorrect should have http:// or https://"}
 				json.NewEncoder(w).Encode(&message)
+				log.Printf("WARN: url format is incorrect")
 			} else {
 				hashUrl, err := usecase.CreateNewUrl(urlInput.OriginUrl)
 				if err != nil {
 					w.WriteHeader(http.StatusInternalServerError)
-					log.Fatal(err.Error())
+					log.Println(err.Error())
 				}
 				urlOutput := UrlOutput{HashUrl: hashUrl}
 				w.WriteHeader(http.StatusCreated)
 				json.NewEncoder(w).Encode(&urlOutput)
+				log.Println("POST/: successfully added new URL")
 
 			}
 		}
