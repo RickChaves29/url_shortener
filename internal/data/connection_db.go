13b15120bce8f2d321c29ace34966cc26b08b3e9
Henrique Chaves <canaldomonza@gmail.com>
2023-02-20 16:28:17 -0300

    refactor: replacing all println to usa log on connection database file

--- internal/data/connection_db.go
+++ internal/data/connection_db.go
@@ -2,22 +2,22 @@ package data
 
 import (
 	"database/sql"
+	"log"
 	"os"
 )
 
 func ConnectionDB() (*sql.DB, error) {
 	db, err := sql.Open("postgres", os.Getenv("CONNECT_DB"))
 	if err != nil {
-		println("error when try open database")
 		panic(err.Error())
 	}
 
 	err = db.Ping()
 	if err != nil {
-		println("error when try connection on database")
+		log.Println("error when try connection on database")
 	}
 
-	println("connection on database is good")
+	log.Println("connection on database is good")
 
 	return db, err
 }
